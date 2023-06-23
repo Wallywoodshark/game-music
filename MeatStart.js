50f24361e317a6c26bbe283a31f476f4987d5c74
Wallywoodshark <136651753+Wallywoodshark@users.noreply.github.com>
2023-06-22 20:30:53 -0400

    i am t
    hey

---
+++ MeatStart.js
@@ -0,0 +1,17 @@
+let topImages = document.querySelector(".types");
+let bottomimage = document.querySelector(".secondPlace")
+let buttonNext2 = document.querySelector("button");
+let topText = document.querySelector(".Topshit");
+
+
+
+
+buttonNext2.onclick=function(){
+    topImages.style.display="none"
+    topText.style.display="none"
+    buttonNext2.style.display='none'
+    bottomimage.style.display="block"
+
+
+
+}   
